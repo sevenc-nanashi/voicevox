045baad12d86caf6b8d61b705cf5a1e5888b08ed
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-11-06 08:23:36 +0900

    Fix: polyfillを修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,36 +3,56 @@ import vue from "@vitejs/plugin-vue";
 import tsconfigPaths from "vite-tsconfig-paths";
 import electron from "vite-plugin-electron";
 import path from "path";
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: `src`,
-  publicDir: `../public`,
-  build: {
-    outDir: `../dist`,
-  },
-  plugins: [
-    vue(),
-    tsconfigPaths(),
-    electron({
-      entry: {
-        background: "background.ts",
-        "electron/preload": "electron/preload.ts",
+export default defineConfig(async () => {
+  const { default: stdLibBrowser } = await import("node-stdlib-browser");
+  return {
+    root: `src`,
+    publicDir: `../public`,
+    build: {
+      outDir: `../dist`,
+    },
+    plugins: [
+      vue(),
+      tsconfigPaths(),
+      electron({
+        entry: {
+          background: "background.ts",
+          "electron/preload": "electron/preload.ts",
+        },
+        vite: {
+          root: `src`,
+          build: {
+            outDir: "../dist",
+          },
+        },
+      }),
+      {
+        ...inject({
+          Buffer: [
+            require.resolve("node-stdlib-browser/helpers/esbuild/shim"),
+            "Buffer",
+          ],
+        }),
       },
-      vite: {
-        root: `src`,
-        build: {
-          outDir: "../dist",
+    ],
+    server: {
+      port: 3000,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        ...stdLibBrowser,
+      },
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: "globalThis",
         },
       },
-    }),
-  ],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
+  };
 });
