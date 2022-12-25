44fbd79af333edcf4da1bff32125941bc0962671
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-12-26 06:47:08 +0900

    Fix: テスト周りを修正

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,14 +1,15 @@
-import { test } from "@playwright/test";
-import { testWithPlaywright } from "vue-cli-plugin-electron-builder";
+import { test, _electron as electron } from "@playwright/test";
 import dotenv from "dotenv";
 
 test("起動したら「利用規約に関するお知らせ」が表示される", async () => {
   dotenv.config(); // FIXME: エンジンの設定直読み
-  const { app, stop } = await testWithPlaywright();
-  const sut = await app.firstWindow();
+  const app = await electron.launch({
+    args: ["dist/background.js"],
+  });
+  const page = await app.firstWindow();
 
   // エンジンが起動し「利用規約に関するお知らせ」が表示されるのを待つ
-  await sut.waitForSelector("text=利用規約に関するお知らせ");
+  await page.waitForSelector("text=利用規約に関するお知らせ");
 
-  await stop();
+  await app.close();
 });
