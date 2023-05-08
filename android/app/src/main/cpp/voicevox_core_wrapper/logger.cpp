6a2c9b0fff517d6612ef59fc25cd0d99747f6edf
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-05-08 19:23:21 +0900

    Refactor: ログ処理を共通化

--- android/app/src/main/cpp/voicevox_core_wrapper/logger.cpp
+++ android/app/src/main/cpp/voicevox_core_wrapper/logger.cpp
@@ -2,46 +2,52 @@
 #include <android/log.h>
 #include <unistd.h>
 
-static int stdoutPfd[2];
-static int stderrPfd[2];
+struct LogParams {
+    int fd;
+    android_LogPriority priority;
+};
+static LogParams stdoutParams;
+static LogParams stderrParams;
 
-static pthread_t thr[2];
-
-static void *logStdoutInBackground(void *rawParams) {
+static void *logInBackground(void *rawParams) {
+    LogParams params = *static_cast<LogParams *>(rawParams);
     ssize_t rdsz;
     char buf[1024];
-    while ((rdsz = read(stdoutPfd[0], buf, sizeof buf - 1)) > 0) {
+    while ((rdsz = read(params.fd, buf, sizeof buf - 1)) > 0) {
         if (buf[rdsz - 1] == '\n') --rdsz;
         buf[rdsz] = 0;
-        __android_log_write(ANDROID_LOG_INFO, "voicevox_core", buf);
-    }
-    return 0;
-}
-static void *logStderrInBackground(void *rawParams) {
-    ssize_t rdsz;
-    char buf[1024];
-    while ((rdsz = read(stderrPfd[0], buf, sizeof buf - 1)) > 0) {
-        if (buf[rdsz - 1] == '\n') --rdsz;
-        buf[rdsz] = 0;
-        __android_log_write(ANDROID_LOG_WARN, "voicevox_core", buf);
+        __android_log_write(params.priority, "voicevox_core", buf);
     }
     return 0;
 }
 
 int startLogger() {
+    int stdoutPfd[2];
+    int stderrPfd[2];
+    pthread_t thr[2];
     pipe(stdoutPfd);
     dup2(stdoutPfd[1], 1);
     pipe(stderrPfd);
     dup2(stderrPfd[1], 2);
 
+    stdoutParams = {
+            .fd = stdoutPfd[0],
+            .priority = ANDROID_LOG_INFO
+    };
+
     if (pthread_create(
-            &thr[0], 0, logStdoutInBackground, nullptr
+            &thr[0], 0, logInBackground, &stdoutParams
     ) == -1)
         return -1;
     pthread_detach(thr[0]);
 
+    stderrParams = {
+            .fd = stderrPfd[0],
+            .priority = ANDROID_LOG_WARN
+    };
+
     if (pthread_create(
-            &thr[1], 0, logStderrInBackground, nullptr
+            &thr[1], 0, logInBackground, &stderrParams
     ) == -1)
         return -1;
     pthread_detach(thr[1]);
